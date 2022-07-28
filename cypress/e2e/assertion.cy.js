79a5afcdacd00cd0865b308c6f578800e5aede8c
Shivani thamke <thamkeshivani200@gmail.com>
2022-07-28 14:53:30 +0530

    assertion example

---
+++ cypress/e2e/assertion.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+it('assertion demo',()=>{
+    cy.visit('https://example.cypress.io')
+    cy.contains('get').click()
+    cy.get('#query-btn')
+      .should('contain','Button')
+      .and('have.class','query-btn')
+      .and('be.visible')
+      .and('be.enabled')
+      expect(true).to.be.true
+      //let name='Querying';
+      //expect(name).to.be.equal.to('Querying')
+    
+})
