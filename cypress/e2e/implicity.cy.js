b4373a716a3c02caa4289377f2d0eb29c1a49671
Shivani thamke <thamkeshivani200@gmail.com>
2022-08-10 12:24:16 +0530

    new commit

--- cypress/e2e/implicity.cy.js
+++ cypress/e2e/implicity.cy.js
@@ -9,5 +9,7 @@ it('implicity assertion',()=>{
     //  cy.contains('SQL SELECT Statement - W3Schools').click()
     // cy.get('[style="width: 600px; transition: height 300ms ease-in-out 0s;"] > .jtfYYd > .jGGQ5e > .yuRUbf > a > .LC20lb').click()
     
-      
+    
+    
+    
 });
