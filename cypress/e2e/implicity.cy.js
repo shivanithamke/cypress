70792b58cca6f77bb485f1941fd81d5d19897232
Shivani thamke <thamkeshivani200@gmail.com>
2022-08-10 11:27:18 +0530

    implicity

---
+++ cypress/e2e/implicity.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+it('implicity assertion',()=>{
+     cy.visit('http://opensource-demo.orangehrmlive.com/')
+     cy.get('#txtUsername').type('Admin')
+     cy.get('#txtPassword').type('admin123')
+     cy.get('#btnLogin').click()
+    //  cy.get('.gLFyf').type('sql select query example{enter}')
+    //  cy.contains('SQL SELECT Statement - W3Schools').click()
+    // cy.get('[style="width: 600px; transition: height 300ms ease-in-out 0s;"] > .jtfYYd > .jGGQ5e > .yuRUbf > a > .LC20lb').click()
+    
+      
+});
